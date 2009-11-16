4dea262b5c85404345c5a81abc8a7fac89c1bef7
Dan Fairs <dan@fezconsulting.com>
2009-11-16 08:29:58 +0000

    Preliminary base and dev settings

--- project/settings.py
+++ project/settings.py
@@ -1,64 +1,95 @@
+# -*- coding: utf-8 -*-
 
-import os
+import os, os.path, sys
+from os.path import join, dirname, normpath
+import re
 
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
-MANAGERS = ADMINS
-
-DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'project.db'
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
-TIME_ZONE = 'America/Chicago'
-
-LANGUAGE_CODE = 'en-us'
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
+PROJECT_ROOT = os.path.dirname(__file__)
+MEDIA_ROOT = join(PROJECT_ROOT, 'media')
 MEDIA_URL = '/media/'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
+STATIC_ROOT = join(MEDIA_ROOT, 'static')
+STATIC_URL = '/media/static/'
 ADMIN_MEDIA_PREFIX = '/admin_media/'
 
-# Don't share this with anybody.
-SECRET_KEY = 'v)(#k@y97svd-$(2h!_*x&-pps64)wcp39dm#t#^7a9#iyjdlg'
+SITE_ID=1
+ROOT_URLCONF = 'mingus.urls'
+TIME_ZONE = 'Europe/London'
+SECRET_KEY = '+bq@r(jph^-*sfj4j%xukecxb0jae9lci&ysy=609hj@3l$47c'
+USE_I18N = False
+HONEYPOT_FIELD_NAME = 'cheezburger_ulike'
+
+MANAGERS = (
+    ('fooper','your@emailaddress'),
+)
+
+TEMPLATE_DIRS = (
+  [os.path.join(PROJECT_ROOT, "templates")]
+)
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.doc.XViewMiddleware',
+    'django.contrib.redirects.middleware.RedirectFallbackMiddleware',
+    'sugar.middleware.debugging.UserBasedExceptionMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'djangodblog.middleware.DBLogMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
-ROOT_URLCONF = 'project.urls'
-
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "basic.blog.context_processors.blog_settings",
+    "mingus.core.context_processors.site_info",
+    "navbar.context_processors.navbars",
+)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.admin',
+    'django.contrib.sitemaps',
+    'django.contrib.flatpages',
+    'django.contrib.redirects',
+
+    'django_extensions',
+    'tagging',
+    'djangodblog',
+    'disqus',
+    'basic.inlines',
+    'basic.blog',
+    'basic.bookmarks',
+    'basic.media',
+    'oembed',
+    'flatblocks',
+    'south',
+    'navbar',
+    'sorl.thumbnail',
+    'template_utils',
+    'django_proxy',
+
+    'django_markup',
+    'google_analytics',
+    'robots',
+    'basic.elsewhere',
+    'compressor',
+    'debug_toolbar',
+    'contact_form',
+    'honeypot',
+    'sugar',
+    'quoteme',
+    'mingus',
 )
 
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+### django-markup
+MARKUP_CHOICES = (
+    'none',
+    'markdown',
+    'textile',
 )
-
-TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
-
-
